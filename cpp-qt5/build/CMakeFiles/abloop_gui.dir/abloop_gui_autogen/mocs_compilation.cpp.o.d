815b725b534b9dd39b0288a6effa17581fedb60d
Iyury1 <iyurychu@ualberta.ca>
2024-05-26 09:20:04 -0600

    removed build folder. added git ignore

--- cpp-qt5/build/CMakeFiles/abloop_gui.dir/abloop_gui_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/abloop_gui.dir/abloop_gui_autogen/mocs_compilation.cpp.o: \
- /home/ian/ABLOOP/GUI/cpp-qt5/build/abloop_gui_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- cpp-qt5/build/CMakeFiles/abloop_gui_autogen.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for abloop_gui_autogen.
